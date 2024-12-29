61003f6d4632a79794cf777e5c18a053e13f788f
JoeyscottSchronce <Joeyscottschronce@gmail.com>
2024-12-29 14:01:36 -0500

    initial commit for react component conversion

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ./Navbar.css;
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                {/* Navigation logo section */}
+                <div className="nav__logo">
+                {/* Link to the home page */}
+                <a href="/" style="text-decoration: none; padding-left: 15px;">
+                    Stay Healthy
+                    {/* Insert an SVG icon of a doctor with a stethoscope */}
+                    <img src="https://cdn.pixabay.com/photo/2021/11/20/03/17/doctor-6810751_1280.png" alt="doctor with a stethoscope" height="35" width="35" style="fill:#3685fb"/>
+                </a>
+                </div>
+                {/* Navigation icon section with an onClick event listener */}
+                <div className="nav__icon" onClick={handleClick}>
+                {/* Font Awesome icon for bars (hamburger menu) */}
+                <i className="fa fa-times fa fa-bars"></i>
+                </div>
+
+                {/* Unordered list for navigation links with 'active' className */}
+                <ul className="nav__links active">
+                {/* List item for the 'Home' link */}
+                <li className="link">
+                    <a href="../Landing_Page/LandingPage.html">Home</a>
+                </li>
+                {/* List item for the 'Appointments' link */}
+                <li className="link">
+                    <a href="#">Appointments</a>
+                </li>
+                {/* List item for the 'Sign Up' link with a button */}
+                <li className="link">
+                    <a href="../Sign_Up//Sign_Up.html">
+                    <button className="btn1">Sign Up</button>
+                    </a>
+                </li>
+                {/* List item for the 'Login' link with a button */}
+                <li className="link">
+                    <a href="../Login/Login.html">
+                    <button className="btn1">Login</button>
+                    </a>
+                </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
