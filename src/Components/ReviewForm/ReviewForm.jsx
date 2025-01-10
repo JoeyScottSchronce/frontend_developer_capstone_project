5cfd289edf1abf4f4712083e7be8fb82d75e2e9a
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-01-09 22:21:36 -0500

    updated review form

--- src/Components/ReviewForm/ReviewForm.jsx
+++ src/Components/ReviewForm/ReviewForm.jsx
@@ -1,41 +1,48 @@
 import React, { useState } from 'react';
-import { DoctorFiles } from '/DoctorCard/DisplayDoctors';
 import './ReviewForm.css';
 
-const data = DoctorFiles;
+const currentReviews = [
+    { id: 1, name: "Dr. Ben Howell", specialty: " General physician", feedback: ' Great work, very helpful', reviewGiven: true },
+    { id: 2, name: "Dr. Ben Howell", specialty: " General physician", feedback: ' Great work, very helpful. Thanks for everything doctor.', reviewGiven: true },
+    { id: 3, name: "Dr. Ben Howell", specialty: " General physician", feedback: ' Great work.', reviewGiven: true },
+    { id: 4, name: "Dr. Ben Howell", specialty: " General physician", feedback: '', reviewGiven: false },
 
-const currentReviews = [{ id: 1, name: "Dr. Ben Howell", specialty: " General physician", feedback: ' Great work, very helpful', reviewGiven: true }]
+]
 
 const ReviewForm = () => {
   const [reviews, setReviews] = useState(currentReviews);
 
-  const handleFeedback = (id) => {
-    alert(`Provide feedback for doctor with ID: ${id}`);
+  const handleFeedback = () => {
+    alert(`Provide feedback for doctor`);
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Serial Number</th>
-          <th>Doctor Name</th>
-          <th>Doctor Specialty</th>
-          <th>Provide feedback</th>
-          <th>Review Given</th>
-        </tr>
-      </thead>
-      <tbody>
-        {reviews.map((review, index) => (
-          <tr key={review.id}>
-            <td>{index + 1}</td>
-            <td>{review.name}</td>
-            <td>{review.specialty}</td>
-            <td><button onClick={() => handleFeedback(review.id)}>Submit Review</button></td>
-            <td>{review.reviewGiven ? 'Yes' : 'No'}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className='review-form-container'>
+        <h1>Reviews</h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Serial Number</th>
+                <th>Doctor Name</th>
+                <th>Doctor Specialty</th>
+                <th className='provide-feedback'>Provide feedback</th>
+                <th>Review Given</th>
+                </tr>
+            </thead>
+            <tbody>
+                {reviews.map((review, index) => (
+                <tr key={review.id}>
+                    <td>{index + 1}</td>
+                    <td>{review.name}</td>
+                    <td>{review.specialty}</td>
+                    <td className='provide-feedback'>{review.feedback}</td>
+                    <td>{review.reviewGiven ? 'Yes' : 'No'}</td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+        <button className='review-form-submit' onClick={() => handleFeedback()}>Submit Review</button>
+    </div>
   );
 };
 
