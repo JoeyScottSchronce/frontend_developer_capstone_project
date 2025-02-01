58d07412cc6b9e1d956796db9e48841a8985cb7b
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-02-01 03:49:38 -0500

    updated route

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/frontend_developer_capstone_project/',
+    base: '/frontend_developer_capstone_project',
     plugins: [react()],
 });
 
